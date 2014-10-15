c5069e52f520d3dae62f92e193624972ce64827b
Novincia <web@novincia.com>
2014-10-15 14:41:47 +0100

    Test Commit Add ViewController

---
+++ SampleProject/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  SampleProject
+//
+//  Created by MOHAMED AHDIDOU on 10/15/14.
+//  Copyright (c) 2014 RESOTEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ SampleProject/TestViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestViewController.m
+//  SampleProject
+//
+//  Created by MOHAMED AHDIDOU on 10/15/14.
+//  Copyright (c) 2014 RESOTEL. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
