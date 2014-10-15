8af481cf76e45d20b8c6bbc7bc5d44b8da250e76
Novincia <web@novincia.com>
2014-10-15 14:43:06 +0100

    Message Obligatoire

---
+++ SampleProject/TestObject.h
@@ -0,0 +1,13 @@
+//
+//  TestObject.h
+//  SampleProject
+//
+//  Created by MOHAMED AHDIDOU on 10/15/14.
+//  Copyright (c) 2014 RESOTEL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+
+@end
---
+++ SampleProject/TestObject.m
@@ -0,0 +1,13 @@
+//
+//  TestObject.m
+//  SampleProject
+//
+//  Created by MOHAMED AHDIDOU on 10/15/14.
+//  Copyright (c) 2014 RESOTEL. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
